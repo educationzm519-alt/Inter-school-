4e36715e809a55a5c7c07993063b2b5b11b43eed
educationzm519-alt <educationzm519@gmail.com>
2026-07-13 13:56:18 +0200

    Create _layout.tsx

---
+++ app/(admin)/app/(admin)/app/(admin)/app/quiz/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router';
+
+export default function QuizLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="[id]" />
+      <Stack.Screen name="results" />
+    </Stack>
+  );
+}
